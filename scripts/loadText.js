c8046de33d84471e285ada69631d502f139d6dd2
edvinasba <edvinas.balciunass@gmail.com>
2017-05-20 16:59:21 +0300

    JSLint suggestions

--- scripts/loadText.js
+++ scripts/loadText.js
@@ -1,54 +1,54 @@
-document.addEventListener('DOMContentLoaded', function() {
-	// Load skillsets
-	loadAjax("JSON/skillset.json");
-	loadAjax("JSON/profile.json");
-	
+/*jslint browser:true */
+/*jslint devel: true */
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Load skillsets
+    loadAjax("JSON/skillset.json");
+    loadAjax("JSON/profile.json");
 }, false);
-
-// Loads json, calls renderHTML with loaded data
-function loadAjax(fileName){
-	var ajaxRequest = new XMLHttpRequest();
-	
-	ajaxRequest.open('GET', fileName);
-	
-	ajaxRequest.onload = function(){
-		var data = JSON.parse(ajaxRequest.responseText);
-		renderHTML(data);
-		return data;
-	};
-
-	ajaxRequest.send();
-}
 
 // Calls html render functions based on object data type
 function renderHTML(data){
-	switch(data.type){
-		case "skills":
-			renderSkillset(data);
-			break;
-		case "profile":
-			renderProfile(data);
-			break;
-	}
+    switch (data.type) {
+    case "skills":
+        renderSkillset(data);
+        break;
+    case "profile":
+        renderProfile(data);
+        break;
+    }
+}
+// Loads json, calls renderHTML with loaded data
+function loadAjax(fileName) {
+    var ajaxRequest = new XMLHttpRequest();
+    ajaxRequest.open('GET', fileName);
+    ajaxRequest.onload = function () {
+        var data = JSON.parse(ajaxRequest.responseText);
+        renderHTML(data);
+        return data;
+    };
+
+    ajaxRequest.send();
 }
 
 // Renders skillsets to id="personalSkills"
 function renderSkillset(data){
-	var htmlString = "";
-	data.data.forEach(function(element) {
-		htmlString += "<article>" + "<h3>" + element.category + "</h3>" + "<ul>";
-		element.skills.forEach(function(element){
-			htmlString += "<li>" + element + "</li>";
-		}, this);
-		htmlString += "</ul>" + "</article>";
-	}, this);
-	document.getElementById("personalSkills").insertAdjacentHTML('beforeend', htmlString);
-	console.log("Skills loaded");
+    var htmlString = "";
+    data.data.forEach(function (element) {
+        htmlString += "<article>" + "<h3>" + element.category + "</h3>" + "<ul>";
+        element.skills.forEach(function (element){
+            htmlString += "<li>" + element + "</li>";
+        }, this);
+        htmlString += "</ul>" + "</article>";
+    }, this);
+    document.getElementById("personalSkills").insertAdjacentHTML('beforeend', htmlString);
+    console.log("Skills loaded");
 }
 
 function renderProfile(data){
-	var htmlString = "";
-	htmlString += '<p class="descriptionText">' + data.data + '</p>';
-	document.getElementById("profile").insertAdjacentHTML('beforeend', htmlString);
-	console.log("Profile loaded");
+    var htmlString = "";
+    htmlString += '<p class="descriptionText">' + data.data + '</p>';
+    document.getElementById("profile").insertAdjacentHTML('beforeend', htmlString);
+    console.log("Profile loaded");
 }
