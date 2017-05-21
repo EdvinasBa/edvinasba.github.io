b8a060c836e0fe61c3d74b11a2660c3e6e64abc8
edvinasba <edvinas.balciunass@gmail.com>
2017-05-21 21:51:46 +0300

    Comments

---
+++ scripts/formatNumber.js
@@ -0,0 +1,11 @@
+// This formats the number
+(function(){
+    var numbers = document.getElementsByClassName("phoneNumber");
+    for (var i = 0; i < numbers.length; i++)
+    {
+        // if( numbers.item(i).innerHTML.slice(4,4) != " ")
+        var text = numbers.item(i).innerHTML.slice(0, 4);
+        var text2 = numbers.item(i).innerHTML.slice(4);
+        numbers.item(i).innerHTML = text + " " + text2;
+    }
+}());
