17d68aeae1c8b6e174e3d3b3c7a98a45333c3f5e
edvinasba <edvinas.balciunass@gmail.com>
2017-05-29 12:20:09 +0300

    Added secrion name to JSON

--- scripts/loadText.js
+++ scripts/loadText.js
@@ -5,13 +5,16 @@
 
     document.addEventListener('DOMContentLoaded', function () {
         // Load skillsets
-        loadAjax("JSON/skillset.json");
-        loadAjax("JSON/profile.json");
-        // loadAjax("JSON/other.json");
-        loadAjax("JSON/experience.json");
-        loadAjax("JSON/education.json");
-        loadAjax("JSON/personality.json");
-        
+        var lang = getParameterByName('lang');
+        var searchQuery = "[data-lang='" + lang + "']";
+        console.log(searchQuery);
+        document.querySelector(searchQuery).classList.add("current");
+
+        loadAjax("JSON_" + lang + "/skillset.json");
+        loadAjax("JSON_" + lang + "/profile.json");
+        loadAjax("JSON_" + lang + "/experience.json");
+        loadAjax("JSON_" + lang + "/education.json");
+        loadAjax("JSON_" + lang + "/personality.json");
         
     }, false);
 
@@ -23,9 +26,6 @@
             break;
         case "profile":
             renderParagraph(data, "profile");
-            break;
-        case "other":
-            renderParagraph(data, "other");
             break;
         case "experience":
             renderDateList(data, "experience");
@@ -51,9 +51,10 @@
         ajaxRequest.send();
     }
 
+
     // Renders skillsets to id="personalSkills"
     function renderSkillset(data) {
-        var htmlString = "";
+        var htmlString = "<div class='personalSkills'>";
         data.data.forEach(function (element) {
             htmlString += "<article>" + "<h3>" + element.category + "</h3>" + "<ul>";
             element.skills.forEach(function (element){
@@ -61,7 +62,12 @@
             }, this);
             htmlString += "</ul>" + "</article>";
         }, this);
-        document.getElementById("personalSkills").insertAdjacentHTML('beforeend', htmlString);
+        htmlString += "</div>"
+        var selectedSection = document.getElementById("personalSkills");
+
+        selectedSection.getElementsByClassName("descriptionTitle")[0].innerHTML = data.name;
+        selectedSection.insertAdjacentHTML('beforeend', htmlString);
+
         console.log("Skills loaded");
     }
 
@@ -69,7 +75,11 @@
     function renderParagraph(data, domID) {
         var htmlString = "";
         htmlString += '<p class="descriptionText">' + data.data + '</p>';
-        document.getElementById(domID).insertAdjacentHTML('beforeend', htmlString);
+        var selectedSection = document.getElementById(domID);
+
+        selectedSection.getElementsByClassName("descriptionTitle")[0].innerHTML = data.name;
+        selectedSection.insertAdjacentHTML('beforeend', htmlString);
+
         console.log("Paragraph loaded - " + domID);
     }
     // Renders a dateList (experience, education, similar) in a given ID
@@ -87,22 +97,57 @@
             htmlString += "</article>";
         }, this);
         htmlString += '</div>';
-        document.getElementById(domID).insertAdjacentHTML('beforeend', htmlString);
+
+        var selectedSection = document.getElementById(domID);
+
+        selectedSection.getElementsByClassName("descriptionTitle")[0].innerHTML = data.name;
+        selectedSection.insertAdjacentHTML('beforeend', htmlString);
+
         console.log("DateList loaded - " + domID);
     }
 
     //leftRightBlocks
     function renderLeftRightBlocks(data){
-        var htmlString = "";
-        data.data.forEach(function(element){
+        var htmlString = "<div class='leftRightBlocks'>";
+        data.data.forEach(function (element){
             htmlString += "<article>";
             htmlString += "<h3><strong>"
             htmlString += element.trait;
             htmlString += "</strong></h3>";
-            htmlString += "<p>" + element.description + "</p>"
-            htmlString += "</article>"
+            htmlString += "<p>" + element.description + "</p>";
+            htmlString += "</article>";
         }, this);
-        document.getElementById("leftRightBlocks").insertAdjacentHTML('beforeend', htmlString);
+        htmlString += "</div>"
+
+        var selectedSection = document.getElementById("leftRightBlocks");
+        selectedSection.getElementsByClassName("descriptionTitle")[0].innerHTML = data.name;
+
+        selectedSection.insertAdjacentHTML('beforeend', htmlString);
+        
         console.log("Left/Right blocks loaded - " + "leftRightBlocks");
+    }
+
+    // Gets parameters from URL
+    function getParameterByName(name, url) {
+        // Get url if no url is given in the parameters
+        if (!url) 
+            url = window.location.href;
+        
+        // Clean up the name
+        name = name.replace(/[\[\]]/g, "\\$&");
+        
+        // ???
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        
+        // ???
+        results = regex.exec(url);
+        
+        // Return results
+        if (!results) 
+            return null;
+        if (!results[2]) 
+            return '';
+        // Decode special chars
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
     }
 })();
